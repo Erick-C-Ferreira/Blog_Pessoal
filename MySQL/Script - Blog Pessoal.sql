ce46f1f5b7fcbfc664a7dc4e08246e17d5eeaf7c
Erick <erickferreira1805@gmail.com>
2022-02-17 20:41:32 -0300

    Blog Pessoal

---
+++ MySQL/Script - Blog Pessoal.sql
@@ -0,0 +1,4 @@
+use bd_blogpessoal;
+-- tb_postagens;
+insert into tb_postagens(data, texto, titulo) values (current_timestamp(), "Blog Pessoal funcionando!", "Minha primeira postagem");
+insert into tb_postagens(data, texto, titulo) values (current_timestamp(), "Blog Pessoal funcionando!", "Minha segunda postagem");
